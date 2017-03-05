ea3c8fc4d77c6541c14b1e3e88147b17134b31df
Ovik <macpro@MacBook-Pro-Mac.local>
2017-03-05 19:50:23 +0300

    2 izmenenie

--- 1Project/ViewController.swift
+++ 1Project/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-var a = Int()
+var a = 14
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
